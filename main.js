6fd62f27ca33d2c9ff5587ac329087bb6b6312c7
Simon Boijer <boijersi@arcada.fi>
2024-10-22 23:37:35 +0300

    FINAL

--- main.js
+++ main.js
@@ -10,25 +10,20 @@ function createWindow () {
     webPreferences: {
       icon: 'assets/images/pixels.ico',
       preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,  // Gör det möjligt att köra Node.js-moduler i renderer-processen
-      contextIsolation: true // Tillåter Electron och Node.js API:er i renderer
+      nodeIntegration: true,  
+      contextIsolation: true 
     },
     autoHideMenuBar: true
   })
 
-  // Load the index.html of the app
   mainWindow.loadFile(path.join(__dirname, 'pages', 'login.html'))
 
-  // Open DevTools automatically (comment out if you don't want it)
-  mainWindow.webContents.openDevTools()
 }
 
-// Called when Electron is ready to create browser windows.
 app.whenReady().then(() => {
   createWindow()
 })
 
-// Example functions for communication between main and renderer (backend/frontend)
 ipcMain.handle('get-stuff-from-main', () => 'Stuff from main!')
 ipcMain.handle('send-stuff-to-main', async (event, data) => console.log(data))
 
